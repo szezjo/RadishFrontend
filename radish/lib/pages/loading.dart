90e09313cc1e1b28d66dc08f883fcdb2f7271663
adacienciala <adacien@o2.pl>
2021-11-12 00:11:16 +0100

    Add basic routing

---
+++ radish/lib/pages/loading.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class LoadingPage extends StatefulWidget {
+  const LoadingPage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<LoadingPage> createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'This is a loading screen.',
+            ),
+            TextButton.icon(
+                onPressed: () {
+                  Navigator.pushNamed(context, "/home");
+                  },
+                icon: const Icon(Icons.home),
+                label: Text("Go Home"))
+          ],
+        ),
+      ),
+    );
+  }
+}
