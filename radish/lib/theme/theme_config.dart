b4fd5a39b6c206cf9b62eea175f30b7bf59c79e8
adacienciala <adacien@o2.pl>
2021-11-12 01:42:44 +0100

    Add rough custom fonts

--- radish/lib/theme/theme_config.dart
+++ radish/lib/theme/theme_config.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 
 class ThemeConfig {
   static Color darkPrimary = const Color(0xFFF24E1E);
+  static Color darkNav = const Color(0xFF272727);
   static Color darkSecondary = const Color(0xFF171717);
   static Color darkAccent = const Color(0xFFF24E1E);
   static Color darkBG = const Color(0xFF202020);
+  static Color darkTextPrimary = const Color(0xFFFFFFFF);
+  static Color darkTextSecondary = const Color(0xFFC4C4C4);
 
   static ThemeData darkTheme = ThemeData(
     colorScheme: ColorScheme(
@@ -23,6 +26,21 @@ class ThemeConfig {
         brightness: Brightness.dark,
     ),
     scaffoldBackgroundColor: darkBG,
-
+    fontFamily: 'DM Sans',
+    textTheme: TextTheme(
+      headline1: TextStyle(fontSize: 72.0, color: darkTextPrimary),
+      headline2: TextStyle(fontSize: 50.0, color: darkTextPrimary),
+      headline3: TextStyle(fontSize: 36.0, color: darkTextPrimary),
+      headline4: TextStyle(fontSize: 24.0, color: darkTextPrimary),
+      headline5: TextStyle(fontSize: 20.0, color: darkTextPrimary),
+      headline6: TextStyle(fontSize: 16.0, color: darkTextPrimary),
+      subtitle1: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500, color: darkTextSecondary),
+      subtitle2: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w500, color: darkTextSecondary),
+      bodyText1: TextStyle(fontSize: 16.0, color: darkTextPrimary),
+      bodyText2: TextStyle(fontSize: 14.0, color: darkTextPrimary),
+      button: TextStyle(fontSize: 14.0, color: darkTextPrimary),
+      caption: TextStyle(fontSize: 14.0, fontStyle: FontStyle.italic, color: darkTextPrimary),
+      overline: TextStyle(fontSize: 14.0, color: darkTextPrimary),
+    )
   );
 }
