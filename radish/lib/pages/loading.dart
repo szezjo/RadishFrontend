b4abdbe2c40c4dfe6ac6dc354ceecb5c8a7dcee9
adacienciala <adacien@o2.pl>
2021-11-15 17:10:24 +0100

    Add loading screen

--- radish/lib/pages/loading.dart
+++ radish/lib/pages/loading.dart
@@ -1,38 +1,61 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:radish/pages/home.dart';
 
 class LoadingPage extends StatefulWidget {
-  const LoadingPage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
   @override
   State<LoadingPage> createState() => _LoadingPageState();
 }
 
 class _LoadingPageState extends State<LoadingPage> {
 
+  int _currIconId = 0;
+  final List<String> _loadingIcons = [
+    'images/loadIcon1.png',
+    'images/loadIcon2.png',
+    'images/loadIcon3.png',
+    'images/loadIcon4.png'
+  ];
+
+  void setupApp() async {
+    var timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        _currIconId++;
+        if (_currIconId >= _loadingIcons.length) {
+          _currIconId = 0;
+        }
+      });
+    });
+    await Future.delayed(const Duration(seconds: 15));
+    timer.cancel();
+    Navigator.pushReplacement(context, PageRouteBuilder(
+      pageBuilder: (c, a1, a2) => const MyHomePage(title: "Homey!"),
+      transitionsBuilder: (c, anim, a2, child) => FadeTransition(opacity: anim, child: child),
+      transitionDuration: Duration(milliseconds: 500),
+    ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupApp();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'This is a loading screen.',
-            ),
-            TextButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, "/home");
-                  },
-                icon: const Icon(Icons.home),
-                label: const Text("Go Home"))
-          ],
-        ),
-      ),
+        backgroundColor: Colors.blue[900],
+        body: AnimatedContainer(
+            duration: const Duration(milliseconds: 1000),
+            child: Center(
+              child: Image.asset(
+              _loadingIcons[_currIconId],
+              height: 150,
+              width: 150,
+            )
+            )
+        )
     );
   }
 }
